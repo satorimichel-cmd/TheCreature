ee3422ed11b733c059a2a46e7d89c6ae7f7c8b47
satorimichel-cmd <satori.michel@gmail.com>
2025-12-06 19:35:56 -0500

    fixes

---
+++ src/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using SlugBase.Features;
+using static SlugBase.Features.FeatureTypes;
+
+namespace SlugTemplate
+{
+    [BepInPlugin(MOD_ID, "The Creature", "0.1.0")]
+    class Plugin : BaseUnityPlugin
+    {
+        private const string MOD_ID = "TheCreatureID";
+
+    }
+}
